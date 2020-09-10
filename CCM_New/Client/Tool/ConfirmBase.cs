0add9dac3068c47be24837eacfa740cae36fe75c
Jaroslaw Granat <jaroslaw.granat@tpvtech0.onmicrosoft.com>
2020-09-10 15:29:12 +0200

    OknoWarning

---
+++ CCM_New/Client/Tool/ConfirmBase.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CCM_New.Client.Tool
+{
+    public class ConfirmBase: ComponentBase
+    {
+        protected bool ShowConfirmation { get; set; }
+
+        [Parameter]
+        public string ConfirmationTitle { get; set; } = "Confirm Delete";
+
+        [Parameter]
+        public string ConfirmationMessage { get; set; } = "Are you sure you want to delete";
+
+        [Parameter]
+        public string ButtonYes { get; set; } = "Delete";
+
+        [Parameter]
+        public string ButtonNo { get; set; } = "Cancek";
+
+        public void Show()
+        {
+            ShowConfirmation = true;
+            StateHasChanged();
+        }
+
+        [Parameter]
+        public EventCallback<bool> ConfirmationChanged { get; set; }
+
+        protected async Task OnConfirmationChange(bool value)
+        {
+            ShowConfirmation = false;
+            await ConfirmationChanged.InvokeAsync(value);
+        }
+    }
+}
+
